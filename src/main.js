85e9dc03fc4184861da0593642aac4ad12376a53
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-06-08 14:59:52 +0100

    router view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+//Import Froala Editor 
+import 'froala-editor/js/plugins.pkgd.min.js';
+//Import third party plugins
+import 'froala-editor/js/third_party/embedly.min';
+import 'froala-editor/js/third_party/font_awesome.min';
+import 'froala-editor/js/third_party/spell_checker.min';
+import 'froala-editor/js/third_party/image_tui.min';
+// Import Froala Editor css files.
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/css/froala_style.min.css';
+import VueFroala from 'vue-froala-wysiwyg';
+import "./index.css";
 
-const app = createApp(App)
 
-app.use(router)
+const app = createApp(App);        
 
-app.mount('#app')
+app.use(VueFroala);
+
+app.use(router);
+
+app.mount("#app");

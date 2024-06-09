4789bceca5e521e470721d3ad81f9d49c05c20e3
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-06-09 15:49:22 +0100

    add image to dashboard articles

--- src/components/BlogPreviewDashboardComponent.vue
+++ src/components/BlogPreviewDashboardComponent.vue
@@ -15,6 +15,10 @@ const props = defineProps({
   id: {
     type: Number,
     required: true
+  },
+  thumbnail: {
+    type: String,
+    required: true
   }
 })
 
@@ -38,6 +42,10 @@ async function handleBlogDelete() {
 <template>
 
   <div class="rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
+    <RouterLink :to="'/blogs/' + id">
+      <img :src="thumbnail" alt="Blog Post" width="400" height="225" class="rounded-t-md"
+        style="aspect-ratio: 400 / 225; object-fit: cover;" />
+    </RouterLink>
     <div class="p-4">
       <h3 class="text-lg font-bold mb-2">{{ title }}</h3>
       <p class="text-gray-500 mb-4">
@@ -46,7 +54,7 @@ async function handleBlogDelete() {
       <div class="flex items-center justify-between">
         <div class="px-2 py-1 rounded-md bg-green-500 text-white">Published</div>
         <div class="flex space-x-2">
-          <button
+          <RouterLink :to="'/blogs/edit/' + id"
             class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5">
@@ -54,7 +62,7 @@ async function handleBlogDelete() {
               <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
               <path d="M10.4 12.6a2 2 0 1 1 3 3L8 21l-4 1 1-4Z"></path>
             </svg>
-          </button>
+          </RouterLink>
           <button @click="handleBlogDelete"
             class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
